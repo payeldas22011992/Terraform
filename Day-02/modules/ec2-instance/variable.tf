b79630ee327664cb2e4924ff7de6964b0df0ec41
payel das <72428413+payeldas22011992@users.noreply.github.com>
2026-01-13 12:47:16 +0530

    Create variable.tf

---
+++ Day-02/modules/ec2-instance/variable.tf
@@ -0,0 +1,15 @@
+
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+}
+
+variable "instance_type_value" {
+  description = "EC2 AMI ID"
+  type        = string
+}
+
+variable "subnet_id_value" {
+  description = "EC2 AMI ID"
+  type        = string
+}
