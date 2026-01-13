3e1616120b9e1cd3edd6cd7992cf4363789f821c
payel das <72428413+payeldas22011992@users.noreply.github.com>
2026-01-13 12:41:43 +0530

    Update main.tf

--- Day-02/modules/ec2-instance/main.tf
+++ Day-02/modules/ec2-instance/main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+}
+
+resource "aws_instance" "my-first-instance" {
+    ami = var.ami_id
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+    associate_public_ip_address = true
+}
