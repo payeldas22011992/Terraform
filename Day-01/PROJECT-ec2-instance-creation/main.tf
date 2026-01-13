e631df22e45ba9bac5adedf50a6e7b7f8a310975
payel das <72428413+payeldas22011992@users.noreply.github.com>
2026-01-13 12:22:32 +0530

    Update main.tf

--- Day-01/PROJECT-ec2-instance-creation/main.tf
+++ Day-01/PROJECT-ec2-instance-creation/main.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+
